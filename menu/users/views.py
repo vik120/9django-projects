262a6e73de9879faf24057a8d9054a1d912987ea
vik120 <saini.vik14@gmail.com>
2024-02-18 21:31:42 +0530

    register

--- menu/users/views.py
+++ menu/users/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from . import forms
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = forms.UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'congrats {username}, you are successfully register')
             return  redirect('food:index')
     else:
         form = UserCreationForm
-        return render(request, 'register.html', context={'form': form})
+        # form = forms.RegisterForm()
+    return render(request, 'register.html', context={'form': form})
