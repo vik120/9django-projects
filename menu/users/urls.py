9e7f00cec0aff018ad61287fa31e489c4241064b
vik120 <saini.vik14@gmail.com>
2024-02-19 23:09:33 +0530

    authentication-login

--- menu/users/urls.py
+++ menu/users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.contrib.auth import  views as authentication_views
+
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('register/', views.register, name="register")
+    path('register/', views.register, name="register"),
+    path('login/', authentication_views.LoginView.as_view(template_name = "login.html"), name="login"),
+    # path('logout/', authentication_views.LogoutView.as_view(template_name="logout.html"), name="logout")
+    path('logout/', views.logout_view, name="logout"),
+    path('profile/', views.profile_page, name="profile")
 ]
--- menu/users/views.py
+++ menu/users/views.py
@@ -1,21 +1,31 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from . import forms
+
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = forms.UserCreationForm(request.POST)
+        form = forms.RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'congrats {username}, you are successfully register')
-            return  redirect('food:index')
+            # return  redirect('food:index')
+            return redirect('users:login')
     else:
-        form = UserCreationForm
-        # form = forms.RegisterForm()
+        # form = UserCreationForm
+        form = forms.RegisterForm()
     return render(request, 'register.html', context={'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('food:index')
+
+@login_required
+def profile_page(request):
+    return render(request, 'profile.html')
